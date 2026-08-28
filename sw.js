175eefe40de3b5d976d77681366eef617d72ffb6
rexram987-create <rexram987@gmail.com>
2026-08-28 13:42:26 +0300

    Bump PWA cache to v9 and include verified overrides

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'street-story-v8';
-const APP_SHELL = ['./', './index.html', './styles.css?v=8', './app.js?v=8', './fixes.js?v=8', './manifest.webmanifest'];
+const CACHE_NAME = 'street-story-v9';
+const APP_SHELL = ['./', './index.html', './styles.css?v=9', './app.js?v=9', './fixes.js?v=9', './verified-overrides.js?v=9', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
