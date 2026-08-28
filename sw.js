61ea7d550922b10a975246ad74a57573c13eda43
rexram987-create <rexram987@gmail.com>
2026-08-28 12:50:09 +0300

    Refresh PWA cache for pilot history data

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'street-story-v1';
+const CACHE_NAME = 'street-story-v2';
 const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
