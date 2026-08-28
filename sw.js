4c7a24c3fdbf2999a2d30f1e41a0d19b47f2144d
rexram987-create <rexram987@gmail.com>
2026-08-28 13:17:12 +0300

    Refresh PWA cache for street matching improvements

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'street-story-v3';
-const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
+const CACHE_NAME = 'street-story-v4';
+const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './fixes.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
